293a840840551c1c55402904ded7dabce7b2f0b8
Shuvra Das <shuvradas59@gmail.com>
2020-07-17 13:36:09 +0600

    add some bootstrap

--- tweets/views.py
+++ tweets/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Tweet
 # Create your views here.
 
-def home_views(request, tweet_id):
-    return HttpResponse(f"<h1> Hello {tweet_id} </h1>")
+def home_view(request):
+   # return HttpResponse("<h1> Hello World </h1>")
+    return render(request, "pages/home.html",context={}, status=200)
+
+def tweet_details_view(request, tweet_id):
+    #obj = Tweet.objects.get(id = tweet_id)
+    return HttpResponse(f"<h1> Hello {tweet_id}</h1>")
